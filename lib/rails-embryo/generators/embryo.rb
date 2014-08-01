434f360823d0fe87427337a21760bddb7d91f7b6
Brian Auton <brianauton@gmail.com>
2014-08-01 13:56:43 -0400

    Move main generator into generators directory

---
+++ lib/rails-embryo/generators/embryo.rb
@@ -0,0 +1,49 @@
+require "rails/generators"
+require "rails/generators/app_base"
+require "embryo/filesystem"
+require "embryo/ruby_version"
+require "embryo/test_support"
+require "embryo/template_support"
+require "embryo/default_view"
+
+class EmbryoGenerator < Rails::Generators::Base
+  def install(force: false, bundle: false)
+    @force = force || options.force?
+    clean_files
+    add_embryo_gem
+    Embryo::RubyVersion.new(filesystem).install
+    Embryo::TestSupport.new(filesystem).install
+    Embryo::TemplateSupport.new(filesystem).install
+    Embryo::DefaultView.new(filesystem).install
+    filesystem.commit_changes
+    update_bundle if bundle
+  end
+
+  private
+
+  def clean_files
+    gemfile.remove_noise
+  end
+
+  def add_embryo_gem
+    filesystem.require_gem "rails-embryo", "~> #{Rails::Embryo::VERSION}"
+  end
+
+  def filesystem
+    @filesystem ||= Embryo::Filesystem.new self, force: @force
+  end
+
+  def gemfile
+    filesystem.gemfile
+  end
+
+  def update_bundle
+    command = "bundle install"
+    if `which bundle`.strip.present?
+      say_status :run, command
+      `#{command}`
+    else
+      say_status :skip, command, :yellow
+    end
+  end
+end
