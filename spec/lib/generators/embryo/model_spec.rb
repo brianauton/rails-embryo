f528df63b806532aa11c1706d882c7fe54ce6d6e
Brian Auton <brianauton@gmail.com>
2014-08-04 10:07:01 -0400

    Reorganize model generator spec

--- spec/lib/generators/embryo/model_spec.rb
+++ spec/lib/generators/embryo/model_spec.rb
@@ -2,30 +2,42 @@ require "generators/embryo/model"
 
 module Embryo
   describe ModelGenerator, :files, :stdout do
-    before do
-      @generator = ModelGenerator.new(["widget"])
-      allow(@generator).to receive :generate
+    describe "invoking rails:model generator" do
+      before do
+        @generator = ModelGenerator.new(["widget"])
+        allow(@generator).to receive :generate
+      end
+
+      it "passes along any command-line arguments" do
+        stub_const "ARGV", ["x", "y"]
+        expect(@generator).to receive(:generate).with "rails:model", a_string_including("x y")
+        @generator.invoke_all
+      end
+
+      it "disables test framework hooks" do
+        expect(@generator).to receive(:generate).with "rails:model", a_string_ending_with("--no-test-framework")
+        @generator.invoke_all
+      end
     end
 
-    it "passes any command-line arguments to rails:model generator" do
-      stub_const "ARGV", ["x", "y"]
-      expect(@generator).to receive(:generate).with "rails:model", a_string_including("x y")
-      @generator.invoke_all
-    end
+    describe "generating a basic model" do
+      before do
+        @generator = ModelGenerator.new(["my_widget"])
+        allow(@generator).to receive :generate
+        @generator.invoke_all
+      end
 
-    it "disables test framework hooks in the rails:model generator" do
-      expect(@generator).to receive(:generate).with "rails:model", a_string_ending_with("--no-test-framework")
-      @generator.invoke_all
-    end
+      it "generates a factory" do
+        expect(File.read "spec/factories/my_widget.rb").to include "factory :my_widget"
+      end
 
-    it "generats a factory" do
-      @generator.invoke_all
-      expect(File.read "spec/factories/widget.rb").to include "factory :widget"
-    end
+      it "generates a spec that describes the model class" do
+        expect(File.read "spec/models/my_widget_spec.rb").to include "describe MyWidget do"
+      end
 
-    it "generates a spec that builds the factory" do
-      @generator.invoke_all
-      expect(File.read "spec/models/widget_spec.rb").to include "build :widget"
+      it "invokes the factory from the spec" do
+        expect(File.read "spec/models/my_widget_spec.rb").to include "build :my_widget"
+      end
     end
   end
 end
